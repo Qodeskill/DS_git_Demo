e5cdf3f24d19d8727dfbce54facfe505a0af886e
paresh_MAC <skillqode.paresh@gmail.com>
2023-02-13 13:07:18 +0530

    First Commit

---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("1Hello");
+    printf("\n2Hello");
+    printf("\n3Hello");
+    return 0;
+}
---
+++ test1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("1Hello");
+    printf("\n2Hello");
+    printf("\n3Hello");
+    printf("\n4Hello");
+    printf("\n5Hello");
+    return 0;
+}
